082fd5d352cd5bf478eeddaeb7fd39cd1d0db0fc
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-05-05 09:27:39 +0000

    [Add]ルーティング、gem追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,45 @@
 Rails.application.routes.draw do
+  # 顧客のルーティング↓
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
   scope module: :public do
-    resources :users, only: [:show, :edit, :update] do
+    root to: "homes#top"
+    get "home/about" => "homes#about", as: "about"
+    resources :users, param: :name, except: [:new, :destroy] do
       member do
         get :likes
       end
+      get "users/unsubscribe" => "users#unsubscribe"
+      get "users/withdraw" => "users#withdraw"
       resource :relationships, only: [:create, :destroy]
       get "followings" => "relationships#followings", as: "followings"
       get "followers" => "relationships#followers", as: "followers"
     end
-    resources :posts
-    resources :favorites, only: [:create, :destroy]
+    resources :posts do
+      resource :favorites, only: [:create, :destroy]
+      resources :comment, only: [:create, :destroy]
+    end
     resources :chats, only: [:show, :create, :destroy]
     get "search" => "searches#search"
   end
-  
-  devise_scope :user do
-    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
-  end
 
-
+  #ここから管理者のルーティング↓
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: :admin do
+    get "/admin" => "homes#top"
+  end
+
+  namespace :admin do
+    resources :posts, except: [:new, :index]
+    resources :users, except: [:new, :destroy]
+    resources :comments, only: [:destroy]
+    get "search" => "searches#search"
+  end
 end
