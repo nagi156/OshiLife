c0588408865a332bf4da2db67c816a851a9ae4ce
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-05-05 12:10:49 +0000

    [Add]ヘッダー作成途中

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,11 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application"; 
+
 require("jquery")
 require("@nathanvda/cocoon")
 
