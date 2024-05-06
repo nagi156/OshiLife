f0fb55a24262892c66e7cd0ab7f9ad9cfd209efa
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-05-06 13:23:47 +0000

    [Add]ヘッダー、ログイン、TOP画面

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
+  before_action :configure_sign_in_params
   # GET /resource/sign_in
   # def new
   #   super
@@ -18,18 +17,19 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
-  
   def after_sign_in_path_for(resource)
     posts_path
   end
-  
+
   def after_sign_out_path_for(resource)
     about_path
   end
+
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :password])
+  end
+
 end
