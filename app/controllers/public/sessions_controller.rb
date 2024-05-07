227fb45f732ce6e3021ea0d422ae0691d9951ee0
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-05-07 13:53:26 +0000

    [Add]ユーザー一覧、詳細、マイページ閲覧、編集可能

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params
+  before_action :user_state, only: [:create]
   # GET /resource/sign_in
   # def new
   #   super
@@ -30,6 +31,17 @@ class Public::SessionsController < Devise::SessionsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :password])
+  end
+  def user_state
+    user = User.find_by(email: params[:user][:email])
+    return if user.nil?
+    return unless user.valid_password?(params[:user][:password])
+    return unless user.is_active?
+      redirect_to posts_path
+      flash[:notice] = "ログインしました。"
+    return
+      render new_user_registration_path
+      flash[:alert] = "退会済みです。再度ご登録をしてご利用ください"
   end
 
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,13 +1,14 @@
 class Public::UsersController < ApplicationController
   before_action :authenticate_user!, except: [:new, :destroy]
+  # before_action :ensure_guest_user, only: [:edit]
 
   def index
     @users = User.all
   end
 
   def my_page
-    @my_page = User.find(current_user.id)
-    @posts = @my_page.posts
+    @user = User.find(current_user.id)
+    @posts = @user.posts
   end
 
   def show
@@ -16,12 +17,39 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
-
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to my_page_path
+      flash[:notice] = "編集しました。"
+    else
+      render :edit
+      flash[:alert] = "編集に失敗しました。"
+    end
+  end
+  
+  # 退会確認画面
+  def unsubscribe
+    @user = current_user
+  end
+  
+  # 退会機能
+  def withdraw
+    @user = current_user
+    # is_activeをfalseにして論理削除する
+    @user.update(is_acttive: false)
+    reset_session
+    flash[:notice] = "退会しました。"
+    redirect_to root_path
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
 end
