40e9647c01378d5e4069ec62f4b8bd4f8d8bd7fa
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-05-09 13:41:34 +0000

    [Add]post編集、削除

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -33,7 +33,7 @@ class Public::PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    if @post.update
+    if @post.update(post_params)
       redirect_to post_path(@post), notice: "編集しました。"
     else
       render :edit
