d820822a3dabed76f26a6c3f82ec6adb8bb42338
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-05-13 09:27:23 +0000

    [Add]検索機能

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,2 +1,13 @@
 class Public::SearchesController < ApplicationController
+  before_action :authenticate_user!
+
+  def search
+    @model = params[:model]
+    @word = params[:word]
+    if @model == "ユーザー"
+      @records = User.search_for(params[:search], @word)
+    else
+      @records = Post.search_for(params[:search], @word)
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -25,16 +25,16 @@ end
 
 
 Post.find_or_create_by!(title: "応援うちわの作り方") do |post|
-  post.complete_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample_post1.png"), filename: "sample_post1.png")
+  post.complete_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample_post4.png"), filename: "sample_post4.png")
 
   material1 = Material.create(name: "ジャンボうちわ黒", amount: 1)
   material2 = Material.create(name: "カラーシールLサイズ(ピンク）", amount: 1)
 
   recipe_instruction1 = Recipe.create(instructions: "うちわを用意し型紙(バック)をカットし型紙をシールに貼ります。")
-  recipe_instruction1.recipe_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample_post2.png"), filename: "sample_post2.png")
+  recipe_instruction1.recipe_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample_post5.png"), filename: "sample_post5.png")
 
   recipe_instruction2 = Recipe.create(instructions: "バックに文字を貼り残りの台紙をゆっくりと剥がしながら、空気が入らないように全体を貼っていきます。")
-  recipe_instruction2.recipe_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample_post3.png"), filename: "sample_post3.png")
+  recipe_instruction2.recipe_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample_post6.png"), filename: "sample_post6.png")
 
   post.recipes << [recipe_instruction1, recipe_instruction2]
   post.materials << [material1, material2]
@@ -45,16 +45,16 @@ end
 
 
 Post.find_or_create_by!(title: "かわいい痛バッグの作り方") do |post|
-  post.complete_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample_post4.png"), filename: "sample_post4.png")
+  post.complete_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample_post1.png"), filename: "sample_post1.png")
 
   material1 = Material.create(name: "缶バッジ", amount: 10)
   material2 = Material.create(name: "痛バッグ用のバッグ", amount: 1)
 
   recipe_instruction1 = Recipe.create(instructions: "好きなキャラクターの缶バッジを用意します。")
-  recipe_instruction1.recipe_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample_post5.png"), filename: "sample_post5.png")
+  recipe_instruction1.recipe_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample_post2.png"), filename: "sample_post2.png")
 
   recipe_instruction2 = Recipe.create(instructions: "バッグのサイズに合わせて付ければ完成です。")
-  recipe_instruction2.recipe_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample_post6.png"), filename: "sample_post6.png")
+  recipe_instruction2.recipe_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample_post3.png"), filename: "sample_post3.png")
 
   post.recipes << [recipe_instruction1, recipe_instruction2]
   post.materials << [material1, material2]
