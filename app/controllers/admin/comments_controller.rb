27aa3c142b9c17995d815c73365d5b64bdb28fce
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-05-15 10:06:43 +0000

    [Add]管理者側のコントローラー、ビューファイル作成中

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,2 +1,15 @@
 class Admin::CommentsController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :set_comment, only: [:destroy]
+  
+  def destroy
+    @comment.destroy
+    redirect_to admin_post_path(params[:post_id]), notice: 'コメントを削除しました。'
+  end
+  
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
 end
