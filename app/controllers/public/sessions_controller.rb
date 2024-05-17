00f7e5b259a554a54bb349a2ecee916bd696e311
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-05-17 02:33:11 +0000

    [Add]レスポンシブ対応可能

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -31,7 +31,7 @@ class Public::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to posts_path, notice: "ゲストさんでログインしました。"
+    redirect_to posts_path, notice: "ゲスト(閲覧専用)でログインしました。"
   end
 
   protected
