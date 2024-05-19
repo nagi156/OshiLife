0341366a42ee207569f63019fadef536ae900ab5
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-05-19 04:12:11 +0000

    [Add]モデル、コントローラー

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,2 +1,16 @@
 class Public::RelationshipsController < ApplicationController
+  befotre_action :authenticate_user!
+  
+  def create
+    following = current_user.relationships.build(following_id: params[:user_id])
+    following.save
+    redirect_to request.referer
+  end
+  
+  def destroy
+    following = current_user.relationships.find_by(following_id: params[:user_id])
+    following.destroy
+    redirect_to request.referer
+  end
+  
 end
