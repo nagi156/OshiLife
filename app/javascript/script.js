df23f7db50295f770426bcfb9dbaa08c2363e2db
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-06-02 12:02:34 +0000

    [Add]言語分析のカスタム

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,13 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // すべての.sentiment-score要素を取得
+document.addEventListener("turbolinks:load", function() {
     let sentimentElements = document.querySelectorAll(".sentiment-score");
 
     sentimentElements.forEach(function(element) {
-        // スコアを取得して、数字として解析する
         let scoreText = element.textContent;
         let score = parseFloat(scoreText.replace('[', '').replace(']', ''));
 
-        // スコアに基づいてクラスを追加
         if (score >= 0) {
             element.classList.add("positive");
         } else {
