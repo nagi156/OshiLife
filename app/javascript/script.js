d9deac92d7469bb5600931edcd746205ada3a524
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-06-03 08:31:56 +0000

    [Add]APIのカスタムとキーの確認

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let sentimentElements = document.querySelectorAll(".sentiment-score");
+
+    sentimentElements.forEach(function(element) {
+        let scoreText = element.textContent;
+        let score = parseFloat(scoreText.replace('[', '').replace(']', ''));
+
+        if (score >= 0) {
+            element.classList.add("positive");
+        } else {
+            element.classList.add("negative");
+        }
+    });
+});
+
 document.addEventListener("turbolinks:load", function() {
     let sentimentElements = document.querySelectorAll(".sentiment-score");
 
