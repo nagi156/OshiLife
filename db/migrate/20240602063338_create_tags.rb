215c00266807616a6f16789f72e06d4e458324ff
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-06-05 03:58:07 +0000

    [Add]余計なデータ削除

--- db/migrate/20240602063338_create_tags.rb
+++
@@ -1,9 +0,0 @@
-class CreateTags < ActiveRecord::Migration[6.1]
-  def change
-    create_table :tags do |t|
-      t.string :name, nill: false
-      t.integer :post_id, nill: false
-      t.timestamps
-    end
-  end
-end
