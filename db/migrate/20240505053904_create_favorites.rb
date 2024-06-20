7a3ce71f573ed9a5577ec8e11d785b2122356719
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-06-20 10:07:01 +0000

    [Add]DBの制約の修正

--- db/migrate/20240505053904_create_favorites.rb
+++ db/migrate/20240505053904_create_favorites.rb
@@ -1,13 +1,11 @@
 class CreateFavorites < ActiveRecord::Migration[6.1]
   def change
     create_table :favorites do |t|
-      t.integer :user_id, null: false
-      t.integer :post_id, null: false
+      t.references :user, foreign_key: true, null: false
+      t.references :post, foreign_key: true, null: false
       t.timestamps
     end
 
     add_index :favorites, [:user_id, :post_id], unique: true
-    add_foreign_key :favorites, :users
-    add_foreign_key :favorites, :posts
   end
 end
